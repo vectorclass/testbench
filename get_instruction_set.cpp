efff032bfe9fcd2fc9dcd85691340a0e7ad3458a
Agner Fog <AgnerF@users.noreply.github.com>
2022-07-20 12:05:55 +0200

    Add files via upload
    Version 2.02.00

--- get_instruction_set.cpp
+++ get_instruction_set.cpp
@@ -1,8 +1,8 @@
 /**********************  get_instruction_set.cpp   ****************************
 * Author:        Agner Fog
 * Date created:  2019-05-08
-* Last modified: 2019-05-08
-* Version:       2.00
+* Last modified: 2022-07-20
+* Version:       2.02.00
 * Project:       vector class library
 * Description:
 * This program outputs the largest instruction set supported on the CPU.
@@ -20,8 +20,10 @@ Output value:
  8  or above = AVX2
  9  or above = AVX512F
 10  or above = AVX512VL, AVX512BW, and AVX512DQ
+11  or above = AVX512VBMI and AVX512VBMI2
+12  or above = AVX512_FP16
 
- (c) Copyright 2019 GNU General Public License 3.0 or later www.gnu.org/licenses
+ (c) Copyright 2019-2022 GNU General Public License 3.0 or later www.gnu.org/licenses
 ******************************************************************************/
 
 
@@ -34,6 +36,8 @@ Output value:
 
 int main() {
     int iset = instrset_detect();
+    if (iset == 10 && hasAVX512VBMI2()) iset = 11;
+    if (iset == 11 && hasAVX512FP16())  iset = 12;
     printf("%i\n", iset);
     return 0;
 }
